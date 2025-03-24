073975cc1fb1ad574ed23bfc13de1190a7aece1b
samsostudios <sam@samsostudios.com>
2025-03-24 11:57:45 -0600

    Updates ++ handoff for maichimp API

--- api/subscribe.ts
+++ api/subscribe.ts
@@ -2,6 +2,17 @@ import fetch from "node-fetch";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://oakleyspirits.webflow.io"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
